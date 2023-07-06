d2842efb89cb2d7dffca6753130f400b6e217a65
Liam Holland <91599612+liamholla@users.noreply.github.com>
2023-07-06 16:20:12 +0100

    Can only add one operator at a time

--- scripts.js
+++ scripts.js
@@ -18,9 +18,12 @@ const divide = function (array) {
     : 0;
 };
 
-let num1 = 1;
-let num2 = 1;
-let operator;
+let numberText1 = "";
+let numberText2 = "";
+let operatorText = "";
+let operatorAllowed = "off";
+let operatorClickCount = 0;
+let displayValue = "";
 
 const operate = function (num1, num2, operator) {
   let result = 0;
@@ -47,22 +50,63 @@ const operate = function (num1, num2, operator) {
 // Display on the calculator
 const calcDisplay = document.querySelector(".display");
 
-// select only the number divs
+// select only the number div
 const numberElements = document.querySelectorAll(".button.number");
 
 // Create a function to call when the user clicks on a number
 const numberClickEventListener = function (event) {
-  console.log(event.target.textContent);
-  let numberText = event.target.textContent;
+  // store the number
+  numberClicked = event.target.textContent;
+  console.log(`The number clicked was ${numberClicked}`);
 
-  calcDisplay.textContent = calcDisplay.textContent + `${numberText}`;
-  numberText = calcDisplay.textContent;
-  console.log(numberText);
+  if (operatorClickCount === 0) {
+    numberText1 = numberText1 + event.target.textContent;
+    //display the number
+    calcDisplay.textContent = calcDisplay.textContent + `${numberClicked}`;
+  } else if (operatorClickCount === 1) {
+    numberText2 = numberText2 + event.target.textContent;
+    //display the number
+    calcDisplay.textContent = calcDisplay.textContent + `${numberClicked}`;
+  }
+
+  console.log(`The number text 1 value is : ${numberText1}`);
+  console.log(`The number text 2 value is : ${numberText2}`);
+  //update operatorAllowed
+  operatorAllowed = "on";
+  console.log(`Operator allowed: ${operatorAllowed}`);
+
+  return numberText1;
+};
+
+// select only the operator divs
+const operatorElements = document.querySelectorAll(".button.operator");
+
+// Create a function to call when the user clicks on a operator
+const operatorClickEventListener = function (event) {
+  operatorClicked = event.target.textContent;
+  console.log(`The operator clicked was ${operatorClicked}`);
+  operatorText = operatorClicked;
+
+  if (operatorAllowed === "on") {
+    operatorClickCount++;
+    console.log(`Operator click count: ${operatorClickCount}`);
+    //add the operator to display
+    calcDisplay.textContent = calcDisplay.textContent + `${operatorClicked}`;
+  }
+  operatorAllowed = "off";
+  console.log(`Operator allowed: ${operatorAllowed}`);
+
+  return operatorText;
 };
 
 // allow clicking on each of the number divs
 numberElements.forEach(function (buttons) {
   buttons.addEventListener("click", numberClickEventListener);
+});
+
+// allow clicking on each of the operator divs
+operatorElements.forEach(function (buttons) {
+  buttons.addEventListener("click", operatorClickEventListener);
 });
 
 /*
